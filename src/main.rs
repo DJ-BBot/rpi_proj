ca50d241c26b8fa8b7d820f8039a1128350b02b8
Roger Barker <pendletonroger@gmail.com>
2021-10-15 08:59:55 -0500

    Created an exit condition with a 'turn off the lights' bit

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::time::Duration;
 fn main() {
     let green = LED::new(18); // set the LED on pin 18 as "green" for the stop light
     let yellow = LED::new(13);
-    let red = LED::new(32); // set a variable for the led on pin 18
+    let red = LED::new(22); // set a variable for the led on pin 18
+    let mut count = 0;
     loop { // start an infinite loop
         green.on();
         sleep(Duration::from_secs(3)); // pause one second
@@ -16,5 +17,13 @@ fn main() {
         red.on();
         sleep(Duration::from_secs(3));
         red.off();
+        if count == 15 {
+            green.off();
+            yellow.off();
+            red.off();
+            break;
+        }
+
+        count = count + 1;
     }
 }
