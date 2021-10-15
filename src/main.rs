e1ebd80d001b5fc80b0d3a490ece5f8a43cf6ec1
Roger Barker <pendletonroger@gmail.com>
2021-10-15 06:48:57 -0500

    Added stoplight functionality. Pin 13: Yellow Pin 18: Green Pin 32: Red
    This will loop forever

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let led = LED::new(18); // set a variable for the led on pin 18
+    let green = LED::new(18); // set the LED on pin 18 as "green" for the stop light
+    let yellow = LED::new(13);
+    let red = LED::new(32); // set a variable for the led on pin 18
     loop { // start an infinite loop
-        led.on();
-        sleep(Duration::from_secs(1)); // pause one second
-        led.off();
-        sleep(Duration::from_secs(1));
+        green.on();
+        sleep(Duration::from_secs(3)); // pause one second
+        green.off();
+        yellow.on();
+        sleep(Duration::from_secs(2));
+        yellow.off();
+        red.on();
+        sleep(Duration::from_secs(3));
+        red.off();
     }
 }
