cf44779dd0491c61fe786314e08f7750728c6bb7
Roger Barker <pendletonroger@gmail.com>
2021-10-19 23:39:16 -0500

    added comments and methods to led.rs including a print out. refactored main a little more

--- src/led.rs
+++ src/led.rs
@@ -2,13 +2,15 @@ use rust_gpiozero::*;
 use std::thread::sleep;
 use std::time::Duration;
 
+// The RbLED struct holds the useful fields for the LEDs that I want to hold
 pub struct RbLED {
-    pub led: rust_gpiozero::LED,
-    pub color: String,
-    pub pin: u8,
+    pub led: rust_gpiozero::LED, //the actual LED object
+    pub color: String, //the color of the physical LED
+    pub pin: u8, //the GPIO pin number
 }
 
 impl RbLED {
+    // Construct a new LED given parameters
     pub fn from(pin: u8, color: String) -> RbLED {
         RbLED {
             led: LED::new(pin),
@@ -16,23 +18,37 @@ impl RbLED {
             pin: pin,
         }
     }
+
+    // Print the LED parameters
+    fn print_led(&self) {
+        let status = match self.led.is_lit() {
+            true => String::from("Active"),
+            false => String::from("Inactive"),
+        };
+        println!("LED:\n\t  Status:\t{}\n\tColor:\t  {}\n\tGPIO Pin: {}\n", 
+            status, self.color, self.pin);
+    }
+
+    // Turn the LED on and off for a set time
     pub fn toggle(&self, secs: Duration) {
-        println!("{} ON!", self.color);
-        self.led.on();
+        self.sw_on();
         sleep(secs);
-        self.led.off();
+        self.sw_off();
     }
     
+    // Switch off the LED
     pub fn sw_off(&self) {
         if self.led.is_lit() {
             self.led.off();
         }
+        self.print_led();
     }
 
+    // Switch on the LED
     pub fn sw_on(&self) {
         if !self.led.is_lit() {
-            println!("{} ON!", self.color);
             self.led.on();
         }
+        self.print_led();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::time::Duration;
 mod led;
 
 fn main() {
-    let mut count = 0;
     let g_led = led::RbLED::from(17, String::from("Green"));
     let y_led = led::RbLED::from(13, String::from("Yellow"));
     let r_led = led::RbLED::from(22, String::from("Red"));
@@ -20,18 +19,10 @@ fn main() {
         led::RbLED::from(26, String::from("Blue")),
     ];
 
-    loop { // start an infinite loop
-        g_led.toggle(Duration::from_secs(3));
-        y_led.toggle(Duration::from_secs(2));
-        r_led.toggle(Duration::from_secs(3));
-        if count == 2 {
-            g_led.sw_off();
-            y_led.sw_off();
-            r_led.sw_off();
-            break;
-        }
-
-        count = count + 1;
+    for _loop_cntr in 1..3 {
+        g_led.toggle(Duration::from_secs(1));
+        y_led.toggle(Duration::from_secs(1));
+        r_led.toggle(Duration::from_secs(1));
     }
 
     println!("Startup is complete!");
