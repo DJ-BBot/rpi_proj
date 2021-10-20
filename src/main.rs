015655b9250acaba0a7b80e6aec82e519e3a4caf
Roger Barker <pendletonroger@gmail.com>
2021-10-19 20:08:47 -0500

    Added printouts when an LED turns on so I can track progress remotely

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let green = LED::new(18); // set the LED on pin 18 as "green" for the stop light
-    let yellow = LED::new(13); // set the LED on pin 13 yellow for the stop light
-    let red = LED::new(22); // set the LED on pin 22 red for the stop light
+    let green = LED::new(17); // set the LED on pin ?? (GPIO 17) as "green" for the stop light
+    let yellow = LED::new(13); // set the LED on pin ?? (GPIO 13) yellow for the stop light
+    let red = LED::new(22); // set the LED on pin ?? (GPIO 22) red for the stop light
     let mut count = 0;
 
     let blues = [
@@ -13,16 +13,16 @@ fn main() {
         LED::new(25), // set the LED on pin 22 (GPIO 25)
         LED::new(12), // set the LED on pin 32 (GPIO 12)
         LED::new(16), // set the LED on pin 36 (GPIO 16)
-        LED::new(5),
-        LED::new(6),
-        LED::new(19),
-        LED::new(26)
+        LED::new(5),  // set the LED on pin ?? (GPIO 5)
+        LED::new(6),  // set the LED on pin ?? (GPIO 6)
+        LED::new(24), // set the LED on pin ?? (GPIO 19)
+        LED::new(26)  // set the LED on pin ?? (GPIO 26)
     ];
 
     loop { // start an infinite loop
-        light_cycle(&green, Duration::from_secs(3));
-        light_cycle(&yellow, Duration::from_secs(2));
-        light_cycle(&red, Duration::from_secs(3));
+        light_cycle(&green, Duration::from_secs(3), String::from("Green"));
+        light_cycle(&yellow, Duration::from_secs(2), String::from("Yellow"));
+        light_cycle(&red, Duration::from_secs(3), String::from("Red"));
         if count == 2 {
             turn_off_lights(&green);
             turn_off_lights(&yellow);
@@ -36,6 +36,7 @@ fn main() {
     println!("Startup is complete!");
 
     for light in blues.iter() {
+        println!("Blue ON!");
         light.on();
         sleep(Duration::from_secs(1));
     }
@@ -44,7 +45,8 @@ fn main() {
     }
 }
 
-fn light_cycle(lt: &rust_gpiozero::LED, secs: Duration) {
+fn light_cycle(lt: &rust_gpiozero::LED, secs: Duration, color: String) {
+    println!("{} ON!", color);
     lt.on();
     sleep(secs);
     lt.off();
